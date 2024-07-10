92290db0895e3f79ff588dab48dbb5187cc78b47
Saif Youssef <seifeldiny1@gmail.com>
2024-07-10 15:47:21 -0600

    created all classes and implemented setters and getters

--- src/seifeldinyoussef/ucalgary/ca/Schedule.java
+++ src/seifeldinyoussef/ucalgary/ca/Schedule.java
@@ -2,4 +2,37 @@ package seifeldinyoussef.ucalgary.ca;
 
 public class Schedule {
 
+	private String startDate;
+	private String endDate;
+	private String clientName;
+	private String sID;
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	public String getClientName() {
+		return clientName;
+	}
+	public void setClientName(String clientName) {
+		this.clientName = clientName;
+	}
+	public String getsID() {
+		return sID;
+	}
+	public void setsID(String sID) {
+		this.sID = sID;
+	}
+	public Schedule( String start, String end, String name) {
+		startDate = start;
+		endDate = end;
+		clientName = name;
+	}
 }
